eb5fe3813ef32425ce470f198727595c86378ba6
Ivanov1201 <vitaliy.ivanov1201@gmail.com>
2022-09-16 17:05:28 -0400

    initial commit

---
+++ precache-manifest.bf1bb513874139dfea191b458b54e7c7.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9fc163ea72454240f2558ad78dbca6",
+    "url": "/backoffice/index.html"
+  },
+  {
+    "revision": "77ee4b37a8a312c4a498",
+    "url": "/backoffice/static/css/3.57195e17.chunk.css"
+  },
+  {
+    "revision": "4842321dcf9bf72cf013",
+    "url": "/backoffice/static/css/main.7386e525.chunk.css"
+  },
+  {
+    "revision": "48d59ec72bd18d5b7bce",
+    "url": "/backoffice/static/js/0.89d823d5.chunk.js"
+  },
+  {
+    "revision": "645931acee1c1d4736be",
+    "url": "/backoffice/static/js/10.82967683.chunk.js"
+  },
+  {
+    "revision": "ec2b295a2e77adfb0f5c",
+    "url": "/backoffice/static/js/11.2ca45879.chunk.js"
+  },
+  {
+    "revision": "77ee4b37a8a312c4a498",
+    "url": "/backoffice/static/js/3.c020c59d.chunk.js"
+  },
+  {
+    "revision": "8377cad274c8ba7d26e7",
+    "url": "/backoffice/static/js/4.6a08deab.chunk.js"
+  },
+  {
+    "revision": "2318ad3da45459abbcd9",
+    "url": "/backoffice/static/js/5.3c8fdb83.chunk.js"
+  },
+  {
+    "revision": "235ce581295a6b61e3c4",
+    "url": "/backoffice/static/js/6.ef02019d.chunk.js"
+  },
+  {
+    "revision": "7bafc72dd69af5e55f03",
+    "url": "/backoffice/static/js/7.777fbf9f.chunk.js"
+  },
+  {
+    "revision": "273b976a10cc8699f1cd",
+    "url": "/backoffice/static/js/8.1dc261f1.chunk.js"
+  },
+  {
+    "revision": "636111b1df837bca0d6c",
+    "url": "/backoffice/static/js/9.2850041e.chunk.js"
+  },
+  {
+    "revision": "4842321dcf9bf72cf013",
+    "url": "/backoffice/static/js/main.2f87b1f7.chunk.js"
+  },
+  {
+    "revision": "e231c8ba92c54e9da0ba",
+    "url": "/backoffice/static/js/runtime-main.e9f520b1.js"
+  },
+  {
+    "revision": "35d544eaaa4cf3c6355866280d53ba73",
+    "url": "/backoffice/static/media/Flaticon.35d544ea.eot"
+  },
+  {
+    "revision": "3e4331ee31764c999add7e0b048c4ba3",
+    "url": "/backoffice/static/media/Flaticon.3e4331ee.ttf"
+  },
+  {
+    "revision": "5be3e43c13c3eb021d15e6682d098d4c",
+    "url": "/backoffice/static/media/Flaticon.5be3e43c.woff"
+  },
+  {
+    "revision": "c1729513a8741b7b61bae040816e426f",
+    "url": "/backoffice/static/media/Flaticon.c1729513.svg"
+  },
+  {
+    "revision": "29586ff0f963f4d1fdfc182822b8b27a",
+    "url": "/backoffice/static/media/Flaticon2.29586ff0.eot"
+  },
+  {
+    "revision": "b242ac810bd8cccaa03abc2128b7c3c3",
+    "url": "/backoffice/static/media/Flaticon2.b242ac81.woff"
+  },
+  {
+    "revision": "e1e2b6e05bbfd279181c693555c61bad",
+    "url": "/backoffice/static/media/Flaticon2.e1e2b6e0.svg"
+  },
+  {
+    "revision": "eafcbac04cdb0a39fe38a36ebd786290",
+    "url": "/backoffice/static/media/Flaticon2.eafcbac0.ttf"
+  },
+  {
+    "revision": "2a5ff834d12f21b421748941d6057d04",
+    "url": "/backoffice/static/media/Ki.2a5ff834.ttf"
+  },
+  {
+    "revision": "31f3692ca10d2f6d0bc467adba4533f7",
+    "url": "/backoffice/static/media/Ki.31f3692c.eot"
+  },
+  {
+    "revision": "451fd5bee09962b6977544325ace633f",
+    "url": "/backoffice/static/media/Ki.451fd5be.svg"
+  },
+  {
+    "revision": "8f9a1a6cffbd1826644e12714ec106d0",
+    "url": "/backoffice/static/media/Ki.8f9a1a6c.woff"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/backoffice/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/backoffice/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/backoffice/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/backoffice/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/backoffice/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/backoffice/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/backoffice/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/backoffice/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/backoffice/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/backoffice/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/backoffice/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/backoffice/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/backoffice/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/backoffice/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/backoffice/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "012e983cf0c81a36a756ca241f43e9b1",
+    "url": "/backoffice/static/media/facebook-3.012e983c.svg"
+  },
+  {
+    "revision": "60e5857089e98edd838074c264d6c951",
+    "url": "/backoffice/static/media/socicon.60e58570.eot"
+  },
+  {
+    "revision": "944f06f5f65ef84a3a36e6c1c2d4b7ad",
+    "url": "/backoffice/static/media/socicon.944f06f5.woff"
+  },
+  {
+    "revision": "9a64ef938f9e55a70a4defc6ac9790bf",
+    "url": "/backoffice/static/media/socicon.9a64ef93.ttf"
+  },
+  {
+    "revision": "a35b65744f557fab5424e99bb6d4e980",
+    "url": "/backoffice/static/media/socicon.a35b6574.svg"
+  },
+  {
+    "revision": "4858941ed86dd9b29412572be29e31b8",
+    "url": "/backoffice/static/media/telegram.4858941e.svg"
+  },
+  {
+    "revision": "c11d008da136b6806125d7a4bfbfdcba",
+    "url": "/backoffice/static/media/twitter.c11d008d.svg"
+  },
+  {
+    "revision": "a0ef7e70ae11ad4701c00e25345f442f",
+    "url": "/backoffice/static/media/verrierEx-logo.a0ef7e70.svg"
+  }
+]);
